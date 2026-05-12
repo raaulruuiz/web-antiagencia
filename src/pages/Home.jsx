07e6895904cc185f2e011e4fbcc070a3c71c1801
Raul Ruiz Romero <raulruizromero@MacBook-Pro-de-Raul.local>
2026-05-12 16:18:44 +0200

    Home: mover br entre 'pero...' y bullets

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -111,9 +111,8 @@ export default function Home() {
               <br />
 
               <p className="mb-4"><strong>¿Qué un niño de 3 años puede ayudarte a vender más?</strong></p>
-              <br />
               <p className="mb-4">Por supuesto no te pido que me creas, pero...</p>
-
+              <br />
               <p className="mb-4">Cuando te suscribes, recibes un ebook de 16 páginas que incluye:</p>
 
               <ul className="mt-2 mb-6 space-y-4 list-none pl-0">
