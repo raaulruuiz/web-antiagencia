24e97b225fde336392a8cc191ac749f17e6900f7
Raul Ruiz Romero <raulruizromero@MacBook-Pro-de-Raul.local>
2026-05-12 16:09:03 +0200

    Home: más interlineado en título, quitar negrita subtítulo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -95,11 +95,11 @@ export default function Home() {
           <div className="bg-white shadow-xl rounded-sm px-8 md:px-16 py-12" style={{ fontFamily: "'Georgia', serif" }}>
             <div className="text-gray-800 text-base leading-relaxed">
 
-              <h1 className="text-3xl md:text-4xl font-bold text-center text-gray-900 leading-normal mb-8">Cómo <span className="underline">aumentar las ventas de tu tienda</span>… con un ejercicio que aprendí de un niño de 3 años y que puedes implementar <span className="underline">en menos de 5 minutos.</span></h1>
+              <h1 className="text-3xl md:text-4xl font-bold text-center text-gray-900 leading-loose mb-8">Cómo <span className="underline">aumentar las ventas de tu tienda</span>… con un ejercicio que aprendí de un niño de 3 años y que puedes implementar <span className="underline">en menos de 5 minutos.</span></h1>
 
               <br />
 
-              <h2 className="text-xl md:text-2xl font-bold text-center text-gray-900 mb-4">En este ebook de <span className="underline">16 páginas</span>, te voy a contar algo que mi sobrino de 3 años no para de decir y que, casi seguro, te va a ayudar a <span className="underline">aumentar las ventas de tu tienda online</span>.</h2>
+              <h2 className="text-xl md:text-2xl font-normal text-center text-gray-900 mb-4">En este ebook de <span className="underline">16 páginas</span>, te voy a contar algo que mi sobrino de 3 años no para de decir y que, casi seguro, te va a ayudar a <span className="underline">aumentar las ventas de tu tienda online</span>.</h2>
 
               <br />
 
